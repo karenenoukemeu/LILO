0fc800637451136b0445168eecdeb65cb2d8694a
karenenoukemeu <92489440+karenenoukemeu@users.noreply.github.com>
2024-03-26 11:42:56 -0500

    Created using Colaboratory

---
+++ test.py
@@ -0,0 +1,41 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyO9inUR7e4WVtEsmpI8U3pV"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "vH1Nj5K0n6oM"
+      },
+      "outputs": [],
+      "source": [
+        "print(\"hello\")\n"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "print(\"model\")"
+      ],
+      "metadata": {
+        "id": "6cwZpyMio8j3"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
